1afcd1af78b55b42a745ce136f2b56789595b6b8
David De La Rosa <daviddelarosa@Davids-MacBook-Pro.local>
2024-03-25 11:06:02 -0500

    changed code to be more compatable

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+// components/About.jsx
+import React from 'react';
+
+const About = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>Your bio or description goes here...</p>
+      {/* Add more details like skills, education, etc. */}
+    </section>
+  );
+};
+
+export default About;
