379627f8b78564d893875fa2f8cde21ff36e9109
David De La Rosa <daviddelarosa@Davids-MacBook-Pro.local>
2024-04-17 16:44:26 -0500

    finished home page, started on about page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,25 +3,28 @@ import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="container">
-      <section>
-        <div>
-          <h1>Let's get down to business.</h1>
-          <ul>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/projects">Projects</Link></li>
-            <li><Link to="/qualifications">Qualifications</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+    <footer id='footer'>
+      <section className="container text-center mb-4 text-white bg-black"> {/* Center the heading and make text white */}
+        <h1 id='footer-title'>Let's get down to business.</h1>
+        <div className="d-flex justify-content-center">
+        <div className='home-about-link'>
+          <Link to="/contact" className="btn btn-lg custom-button about-me-text">Message me</Link>
+        </div>
+      </div>
+
+      </section>
+      <div className="row justify-content-start"> {/* Align list to the left */}
+        <div className="col-md-6"> {/* Adjust column size as needed */}
+          <ul className="list-unstyled text-white bg-black"> {/* Make list text white */}
+            <li><Link to="/about" className="text-white">About</Link></li>
+            <li><Link to="/projects" className="text-white">Projects</Link></li>
+            <li><Link to="/qualifications" className="text-white">Qualifications</Link></li>
+            <li><Link to="/contact" className="text-white">Contact</Link></li>
           </ul>
         </div>
-      </section>
       </div>
     </footer>
   );
 };
 
 export default Footer;
-
-
-
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,65 +1,69 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 const Home = () => {
   return (
     <div id="home">
-  <section id="home-header" class="container">
-    <div class="row">
-      <div class="col-md-8 col-sm-10 col-xs-12">
-        <div class="">
-          <h1 className='home-header-text'>A Full Stack Developer skilled in front-end and back-end development.</h1>
+      <section id="home-header" className="container">
+        <div className="row">
+          <div className="col-md-8 col-sm-10 col-xs-12">
+            <div>
+              <h1 className='home-header-text'>A Full Stack Developer skilled in front-end and back-end development.</h1>
+            </div>
+          </div>
         </div>
+      </section>
+      
+      <section id="home-picture" className="d-flex justify-content-center align-items-center">
+  <div className="container">
+    <div className="row">
+      <div className="col-md-12 d-flex justify-content-center">
+        <img src="/pfp.png" alt="Photo of David De La Rosa" />
       </div>
     </div>
-  </section>
-   
-  <section id="home-picture" className="d-flex justify-content-center align-items-center">
-  <div>      
-    <img src="/pfp.png" alt="Photo of David De La Rosa" />
   </div>
 </section>
 
-    <section id="home-quote" className="container-fluid">
-      <div className="row">
-        <div className="col-md-6 col-sm-12">
-          <div className='home-quote-text'>
-            <h3>So, you're pouring your heart into a project,</h3>
-            <h3>but how do you ensure users fall in love with it too?</h3>
-            <h3 className='twici'>That's where I come in as a full-stack developer!</h3>
+
+      <section id="home-quote" className="container-fluid">
+        <div className="row">
+          <div className="col-md-6 col-sm-12">
+            <div className='home-quote-text'>
+              <h3>So, you're pouring your heart into a project,</h3>
+              <h3>but how do you ensure users fall in love with it too?</h3>
+              <h3 className='twici'>That's where I come in as a full-stack developer!</h3>
+            </div>
           </div>
+        </div>
+      </section>
+
+      <div className="row justify-content-start">
+  <div className="col-md-6 col-sm-12 d-flex justify-content-start" style={{ marginLeft: '60px' }}> {/* Make it occupy the entire width and push its content to the left */}
+    <div className='home-about-link'> {/* Apply margin to the right */}
+      <Link to="/about" className="btn btn-lg custom-button about-me-text">About Me</Link>
+    </div>
+  </div>
+</div>
+
+
+
+
+      <section id='home-projects' className="container-fluid d-flex justify-content-center align-items-center">
+        <div className="row">
+          <div className="col-lg-8 col-md-10 col-sm-12 text-center">
+            <div className='home-projects-quote'>
+              <h2 className='fsd'>I've spearheaded numerous full-stack developer projects that could reach and benefit thousands of users.</h2>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="projects-link col-md-6 col-sm-12 d-flex justify-content-center">
+        <div className='project-about-link'>
+          <Link to="/projects" className="btn btn-lg custom-button about-me-text">Projects</Link>
         </div>
       </div>
-    </section>
-
-    <div className="col-md-6 col-sm-12 d-flex">
-          <div className='home-about-link'>
-          <button type="button" class="btn btn-lg custom-button">
-            <Link to="/about" className="about-me-text">About Me</Link>
-          </button>
-          </div>
-        </div>
-
-      <section id='home-projects'>
-        <div className='home-projects-quote'>
-          <h2>I've spearheaded numerous full-stack developer projects- </h2>
-          <h2>that could reach and benefited thousands of users.</h2>
-          <Link to="/projects">Projects</Link>
-        </div>
-      </section>
-
-      <section>
-        <div>
-          <h2>Featured Projects</h2>
-          <p>About the project</p>
-          <Link to="/projects">View Case Study</Link>
-        </div>
-      </section>
-
-
-
     </div>
   );
 };
