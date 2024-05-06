63ea2205f5bcf6bfe3fe02fd6e5c7bf343e4363c
David De La Rosa <daviddelarosa@Davids-MacBook-Pro.local>
2024-05-06 14:30:15 -0500

    Completed about me page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,23 @@
-// components/About.jsx
 import React from 'react';
 
 const About = () => {
   return (
-    <section id="about">
-      <h2>About Me</h2>
-      <p>Your bio or description goes here...</p>
-      {/* Add more details like skills, education, etc. */}
-    </section>
+    <div className='page'>
+      <section id="about-me-bio" className="container-fluid d-flex justify-content-center align-items-center">
+        <h2 className='about-me-heading text-center'>As a full-stack developer, I manage all aspects of web application development, from UI design to backend logic and databases.</h2>
+      </section>
+      <section className="container">
+        <div className="row" id='bio-section'>
+          <div className="col-md-6 bg-white" id='bio'>
+            <h2 id='about-me-h2'>But that’s not all…</h2>
+            <p id='about-me-p' className=''>Welcome! I'm David De La Rosa, a Full-Stack Developer certified by UTSA in 2024. With a passion for coding since 2017, I've specialized in creating seamless web experiences. My journey began by supporting my family's business, where I've optimized our online presence. I thrive on challenges and love exploring new technologies. Let's connect and bring your digital ideas to life!</p>
+          </div>
+          <div className="col-md-6">
+            <img src="/wedding.png" alt="wedding-img" className="img-fluid" id='wedding-img' />
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,40 +2,16 @@
 import React from 'react';
 
 const Projects = () => {
-  // Sample project data
-  const projects = [
-    {
-      title: 'Project 1',
-      description: 'Description of Project 1',
-      image: 'project1.jpg', // Image file should be located in your public folder
-      link: 'https://github.com/example/project1' // Link to project or source code
-    },
-    {
-      title: 'Project 2',
-      description: 'Description of Project 2',
-      image: 'project2.jpg',
-      link: 'https://github.com/example/project2'
-    }
-    // Add more projects as needed
-  ];
 
   return (
     <section id="projects">
-      <div className="container">
-        <h2>My Projects</h2>
-        <div className="projects-grid">
-          {projects.map((project, index) => (
-            <div className="project" key={index}>
-              <img src={project.image} alt={project.title} />
-              <div className="project-info">
-                <h3>{project.title}</h3>
-                <p>{project.description}</p>
-                <a href={project.link} target="_blank" rel="noopener noreferrer">View Project</a>
-              </div>
-            </div>
-          ))}
-        </div>
+      <h2>Projects</h2>
+      <div className="project">
+        <h3>Project Title</h3>
+        <p>Project description...</p>
+        <a href="/project-url">Project URL</a>
       </div>
+      {/* Add more projects here */}
     </section>
   );
 };
