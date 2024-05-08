a22ed17e1562dd3d750a2640ebc2da7fc34e7815
David De La Rosa <daviddelarosa@Davids-MacBook-Pro.local>
2024-05-08 11:21:02 -0500

    Added Jquery and fisnished projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,18 +1,79 @@
-// components/Projects.jsx
 import React from 'react';
 
 const Projects = () => {
 
   return (
-    <section id="projects">
-      <h2>Projects</h2>
-      <div className="project">
-        <h3>Project Title</h3>
-        <p>Project description...</p>
-        <a href="/project-url">Project URL</a>
-      </div>
-      {/* Add more projects here */}
-    </section>
+    <div id='projects-page'>
+      <section id="fphc" className="container-fluid d-flex justify-content-center align-items-center">
+        <h2 id='featured-project' className='text-center'>Featured Projects</h2>
+      </section>
+      <section id='project-cards'>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6" id='ET-card'>
+              <div className="card">
+                <img src="/first-project.png" alt="first-project-img" className="card-img-top" />
+                <div className="card-body">
+                  <h5 className="card-title">Employee Tracker</h5>
+                  <p className="card-text">This project was to make a simple employee database.</p>
+                  <a href="https://github.com/daviddelarosa210/Employee-Tracker" className="btn btn-primary">View Project</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6" id='ET-card' >
+              <div className="card">
+                <img src="/second-project.png" alt="second-project-img" className="card-img-top" />
+                <div className="card-body">
+                  <h5 className="card-title">E-commerce Back End</h5>
+                  <p className="card-text">This project was to connect the back end of a E-commerce website.</p>
+                  <a href="https://github.com/daviddelarosa210/E-commerce-Back-End" className="btn btn-primary">View Project</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6" id='ET-card'>
+              <div className="card">
+                <img src="/third-project.png" alt="third-project-img" className="card-img-top" />
+                <div className="card-body">
+                  <h5 className="card-title">Weather Jamz</h5>
+                  <p className="card-text">receive music recommendations based on the weather using a weather API and the Spotify music API.</p>
+                  <a href="" className="btn btn-primary">View Project</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6" id='ET-card'>
+              <div className="card">
+                <img src="/fourth-project.png" alt="fourth-project-img" className="card-img-top" />
+                <div className="card-body">
+                  <h5 className="card-title">Social Network</h5>
+                  <p className="card-text">This is a RESTful API for a social network. It allows users to perform various operations, including creating and managing users, thoughts, and reactions.</p>
+                  <a href="https://github.com/daviddelarosa210/Social-Network-API" className="btn btn-primary">View Project</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6" id='ET-card'>
+              <div className="card">
+                <img src="" alt="html-website-img" className="card-img-top" />
+                <div className="card-body">
+                  <h5 className="card-title">Read Me Generator</h5>
+                  <p className="card-text">This project generates a read me and inserts it directly into your project file.</p>
+                  <a href="https://github.com/daviddelarosa210/Read-Me-Generator" className="btn btn-primary">View Project</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6" id='ET-card'>
+              <div className="card">
+                <img src="" alt="password-gen-img" className="card-img-top" />
+                <div className="card-body">
+                  <h5 className="card-title">Password Generator</h5>
+                  <p className="card-text">Password Generator: effortlessly create strong, tailored passwords.</p>
+                  <a href="https://daviddelarosa210.github.io/Password-Generator/" className="btn btn-primary">View Project</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
--- src/components/Qualifications.jsx
+++ src/components/Qualifications.jsx
@@ -1,33 +1,29 @@
-// components/Qualifications.jsx
-import React from 'react';
+import React, { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // This file includes both Bootstrap JavaScript and jQuery
+// Importing $ from jQuery is not necessary here because it's already included in bootstrap.bundle.min.js
 
 const Qualifications = () => {
+  useEffect(() => {
+    // Bootstrap dropdowns should be initialized automatically by bootstrap.bundle.min.js, so you don't need to manually call it
+  }, []);
+
   return (
     <section id="qualifications">
       <div className="container">
-        <h2>Qualifications</h2>
-        <div className="qualification">
-          <h3>Education</h3>
-          <ul>
-            <li>Bachelor's Degree in Computer Science - University Name, Year</li>
-            {/* Add more education qualifications as needed */}
-          </ul>
-        </div>
-        <div className="qualification">
-          <h3>Skills</h3>
-          <ul>
-            <li>JavaScript (ES6+)</li>
-            <li>React.js</li>
-            <li>HTML5 & CSS3</li>
-            {/* Add more skills as needed */}
-          </ul>
-        </div>
         <div className="qualification">
           <h3>Certifications</h3>
-          <ul>
-            <li>Certified React Developer - Certification Body, Year</li>
-            {/* Add more certifications as needed */}
-          </ul>
+          <div className="dropdown">
+            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              View Certifications
+            </button>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a className="dropdown-item" href="#">JavaScript (ES6+)</a>
+              <a className="dropdown-item" href="#">React.js</a>
+              <a className="dropdown-item" href="#">HTML5 & CSS3</a>
+              {/* Add more certifications as needed */}
+            </div>
+          </div>
         </div>
       </div>
     </section>
