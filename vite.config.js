c5ba6ed018b20bd2a25ac1e4383e31269007852b
David De La Rosa <daviddelarosa@Davids-MacBook-Pro.local>
2024-07-08 22:36:18 -0500

    deleted server, not needed

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true
-  }
-})
+    port: 3000, // Specify the port you want to use
+    // Remove 'open: true' as it's not needed for production deployment
+  },
+});
